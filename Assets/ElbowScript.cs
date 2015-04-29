4c7039fbd56749b2e6ca03a6d1606d59d0979a56
eduardojfp <eduardojali@gmail.com>
2015-04-29 15:44:36 -0400

    Actual Rotations

--- Assets/ElbowScript.cs
+++ Assets/ElbowScript.cs
@@ -3,23 +3,34 @@ using System.Collections;
 
 public class ElbowScript : MonoBehaviour {
 
-	Vector3 move;
-	Vector3 axis;
-	ConfigurableJoint myJoint;
-	float rotateSpeed;
+	// The elbow has only one degree of freedom
+	// so only one rotation axis is needed
+	public float yRot;
+
+	// Min and max rotation for the elbow
+	public float yMin, yMax;
+
+	// Rotation Speed
+	public float rotSpeed;
 
 	// Use this for initialization
 	void Start () {
-		move = new Vector3(1,1,1);
-		axis = new Vector3 (10f, 0, 0);
-		rotateSpeed = 5f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.D)) {
-			gameObject.transform.Rotate(Vector3.up * 5, Space.World);
-			Debug.Log("Rotating Elbow");
+		if(Input.GetKey(KeyCode.Z)) {
+			Debug.Log("Rotating Elbow over Y Positively");
+			yRot += rotSpeed;
 		}
+
+		if(Input.GetKey(KeyCode.X)) {
+			Debug.Log("Rotating Elbow over Y Negatively");
+			yRot -= rotSpeed;
+		}
+
+		yRot = Mathf.Clamp(yRot, yMin, yMax);
+		gameObject.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
 	}
 }
--- Assets/ShoulderMove.cs
+++ Assets/ShoulderMove.cs
@@ -3,37 +3,70 @@ using System.Collections;
 
 public class ShoulderMove : MonoBehaviour {
 
-	public Transform arm1;
+	// These floats will represent the Shoulder's
+	// rotation in 3D space. They are public so 
+	// they can be changed in the editor. The 
+	// Shoulder has 3 degrees of freedom, so x, y,
+	// and z axis are neccessary.
+	public float xRot;
+	public float yRot;
+	public float zRot;
 
-	Vector3 move;
-	Vector3 axis;
-	ConfigurableJoint myJoint;
-	float rotateSpeed;
+	// These are the min and max rotations the shoulder can achieve.
+	// These values will be used for clamping rotation. They
+	// are public so they can be changed in the editor.
+	public float xMin, xMax;
+	public float yMin, yMax;
+	public float zMin, zMax;
 
-	// Rotation floats
-	float yRot = 0;
-	float minRot = -45f;
-	float maxRot = 45f;
+	// Speed at which objects are manually rotated
+	public float rotSpeed;
 
 	// Use this for initialization
 	void Start () {
-		move = new Vector3(1,1,1);
-		axis = new Vector3 (10f, 0, 0);
-		rotateSpeed = 5f;
 
-		yRot = 3;
-		yRot = Mathf.Clamp (yRot, minRot, maxRot);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		gameObject.transform.eulerAngles = new Vector3 (gameObject.transform.eulerAngles.x, yRot, gameObject.transform.eulerAngles.z);
-		gameObject.transform.Rotate (yRot, 0, 0);
-
-		// Rotation Controls
+	
+		// Manual Rotation Controls
 		if(Input.GetKey(KeyCode.A)) {
-			Debug.Log("Rotating Shoulder");
+			Debug.Log("Rotating Shoulder over X Positively");
+			xRot += rotSpeed;
 		}
+
+		if(Input.GetKey(KeyCode.S)) {
+			Debug.Log("Rotating Shoulder over Y Positively");
+			yRot += rotSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.D)) {
+			Debug.Log("Rotating Shoulder over Z Positively");
+			zRot += rotSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.Q)) {
+			Debug.Log("Rotating Shoulder over X Negatively");
+			xRot -= rotSpeed;
+		}
+		
+		if(Input.GetKey(KeyCode.W)) {
+			Debug.Log("Rotating Shoulder over Y Negatively");
+			yRot -= rotSpeed;
+		}
+		
+		if(Input.GetKey(KeyCode.E)) {
+			Debug.Log("Rotating Shoulder over Z Negatively");
+			zRot -= rotSpeed;
+		}
+
+		// Clamping controls
+		xRot = Mathf.Clamp(xRot, xMin, xMax);
+		yRot = Mathf.Clamp(yRot, yMin, yMax);
+		zRot = Mathf.Clamp(zRot, zMin, zMax);
+
+		// Actual rotation command;
+		transform.localEulerAngles = new Vector3(xRot, yRot, zRot);
 	}
 }
