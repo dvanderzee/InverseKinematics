f768a3bd27d0afc3117549f7bb376ef17f3669b0
eduardojfp <eduardojali@gmail.com>
2015-05-08 17:40:09 -0400

    Started working with new Jacobian calculation

--- Assets/ShoulderMove.cs
+++ Assets/ShoulderMove.cs
@@ -26,8 +26,8 @@ public class ShoulderMove : MonoBehaviour {
 
 
 	// Matrices to hold our Jacobian Values and it's Adjacency Matrix
-	float[,] jacobian = new float[3, 2];
-	float[,] psuedo = new float[2, 3];
+	float[,] jacobian = new float[3, 6];
+	float[,] psuedo = new float[6, 3];
 
 	// Speed at which objects are manually rotated
 	public float rotSpeed;
@@ -60,7 +60,40 @@ public class ShoulderMove : MonoBehaviour {
 
 	// (Axis of Revolution) X (Vector from joint to end effector)
 	void jacobianCalculation() {
+		Vector3 xAxis = new Vector3 (1, 0, 0);
+		Vector3 yAxis = new Vector3 (0, 1, 0);
+		Vector3 zAxis = new Vector3 (0, 0, 1);
 
+		Vector3 shoulderDist = (hand.transform.position - transform.position);		// Vector from Shoulder to Hand
+		Vector3 elbowDist = (hand.transform.position - elbow.transform.position);	// Vector from Elbow to Hand
+
+		Vector3 shoulderCol1 = Vector3.Cross (xAxis, shoulderDist);
+		Vector3 shoulderCol2 = Vector3.Cross (yAxis, shoulderDist);
+		Vector3 shoulderCol3 = Vector3.Cross (zAxis, shoulderDist);
+
+		Vector3 elbowCol1 = Vector3.Cross (xAxis, elbowDist);
+		Vector3 elbowCol2 = Vector3.Cross (yAxis, elbowDist);
+		Vector3 elbowCol3 = Vector3.Cross (zAxis, elbowDist);
+
+		jacobian[0,0] = shoulderCol1.x;
+		jacobian[0,1] = shoulderCol2.x;
+		jacobian[0,2] = shoulderCol3.x;
+		jacobian[0,3] = elbowCol1.x;
+		jacobian[0,4] = elbowCol2.x;
+		jacobian[0,5] = elbowCol3.x;
+		jacobian[1,0] = shoulderCol1.y;
+		jacobian[1,1] = shoulderCol2.y;
+		jacobian[1,2] = shoulderCol3.y;
+		jacobian[1,3] = elbowCol1.y;
+		jacobian[1,4] = elbowCol2.y;
+		jacobian[1,5] = elbowCol3.y;
+		jacobian[2,0] = shoulderCol1.z;
+		jacobian[2,1] = shoulderCol2.z;
+		jacobian[2,2] = shoulderCol3.z;
+		jacobian[2,3] = elbowCol1.z;
+		jacobian[2,4] = elbowCol2.z;
+		jacobian[2,5] = elbowCol3.z;
+		/*
 		elbowX = elbowscript.xRot;
 		elbowY = elbowscript.yRot;
 		elbowZ = elbowscript.zRot;
@@ -72,7 +105,6 @@ public class ShoulderMove : MonoBehaviour {
 			Mathf.Sqrt (Mathf.Pow (xRot, 2) + Mathf.Pow (yRot, 2) + Mathf.Pow (zRot, 2));
 		float elbowRotMag = 	// Magnitude of the Elbow Rotation
 			Mathf.Sqrt (Mathf.Pow (elbowX, 2) + Mathf.Pow (elbowY, 2) + Mathf.Pow (elbowZ, 2));
-
 
 		shoulderAxis = (shoulderRot / shoulderRotMag);
 		elbowAxis = (elbowRot / elbowRotMag);
@@ -95,21 +127,41 @@ public class ShoulderMove : MonoBehaviour {
 		Debug.Log("Jacobian is:");
 		Debug.Log("Col 1: " + jacobian[0,0] + " " + jacobian[1,0] + " " + jacobian[2,0]);
 		Debug.Log("Col 2: " + jacobian[0,1] + " " + jacobian[1,1] + " " + jacobian[2,1]);
+		*/
 
 	}
 	// PsuedoInverse = Inverse(Transpose * Jacobian) * Transpose
 	void pseudoInverse() {
-		float[,] transpose = new float[2, 3];
+		float[,] transpose = new float[6, 3];
 
+		//computes the transpose
 		transpose [0, 0] = jacobian [0, 0];
 		transpose [0, 1] = jacobian [1, 0];
 		transpose [0, 2] = jacobian [2, 0];
+		transpose [0, 3] = jacobian [3, 0];
+		transpose [0, 4] = jacobian [4, 0];
+		transpose [0, 5] = jacobian [5, 0];
 		transpose [1, 0] = jacobian [0, 1];
 		transpose [1, 1] = jacobian [1, 1];
 		transpose [1, 2] = jacobian [2, 1];
+		transpose [1, 3] = jacobian [3, 1];
+		transpose [1, 4] = jacobian [4, 1];
+		transpose [1, 5] = jacobian [5, 1];
+		transpose [2, 0] = jacobian [0, 2];
+		transpose [2, 1] = jacobian [1, 2];
+		transpose [2, 2] = jacobian [2, 2];
+		transpose [2, 3] = jacobian [3, 2];
+		transpose [2, 4] = jacobian [4, 2];
+		transpose [2, 5] = jacobian [5, 2];
 	
-		float[,] temp = new float[2, 2];
-		float[,] tempinv = new float[2, 2];
+		float[,] temp = new float[3, 3];
+		float[,] tempinv = new float[3, 3];
+
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				//temp[i][j] = transpose[i,j]*jacobian[i,j] + transpose[i+1,j]*jacobian[
+			}
+		}
 
 		temp [0, 0] = (transpose [0, 0] * jacobian [0, 0]) + (transpose [0, 1] * jacobian [1, 0]) + (transpose [0, 2] * jacobian [2, 0]);
 		temp [0, 1] = (transpose [0, 0] * jacobian [0, 1]) + (transpose [0, 1] * jacobian [1, 1]) + (transpose [0, 2] * jacobian [2, 1]);
@@ -157,7 +209,7 @@ public class ShoulderMove : MonoBehaviour {
 		elbowscript.yRot += (deltaTheta [1, 0] * step);
 		elbowscript.zRot += (deltaTheta [1, 0] * step);
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 
